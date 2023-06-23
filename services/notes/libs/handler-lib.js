5a68fc0e32636c660088322b6a5ed0c4623c98df
ShreetamMishra <shreetammishra1@gmail.com>
2023-06-23 22:04:04 +0530

    First comi8t

--- services/notes/libs/handler-lib.js
+++ services/notes/libs/handler-lib.js
@@ -1,28 +1,23 @@
-
-
 export default function handler(lambda) {
     return async function (event, context) {
-        let body, statusCode;
-
-        debug.init(event, context);
-
-        try {
-            body = await lambda(event, context);
-            statusCode = 200;
-        } catch (e) {
-            debug.flush(e);
-
-            body = {error: e.message};
-            statusCode = 500;
+      let body, statusCode;
+      try {
+        // Run the Lambda
+        body = await lambda(event, context);
+        statusCode = 200;
+      } catch (e) {
+        console.log(e);
+        body = { error: e.message };
+        statusCode = 500;
+      }
+    // Return HTTP response
+      return {
+        statusCode,
+        body: JSON.stringify(body),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
         }
-
-        return {
-            statusCode,
-            body: JSON.stringify(body),
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": true,
-            },
-        };
     };
+  };
 }
